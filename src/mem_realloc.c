52422f5727822513aaa674c26bd459d6f9acac81
Nikita <ybrbnfrhen@ukr.net>
2019-02-25 22:14:43 +0200

    fix max issue

--- src/mem_realloc.c
+++ src/mem_realloc.c
@@ -2,13 +2,14 @@
 
 void			*mem_realloc(void *addr, size_t size)
 {
-    t_memlist *old_node;
-    t_memlist *new_node;
+    t_memlist   *old_node;
+    t_memlist   *new_node;
+    void        *data;
 
     if(!(old_node = find_node(addr)) || !size)
         return (NULL);
-    new_node = mem_alloc(size);
-    new_node->addr = memmove(new_node->addr, old_node->addr,
+    data = mem_alloc(size);
+    new_node->addr = memmove(data, old_node->addr,
                             old_node->size > size ? size : old_node->size);
     return (new_node->addr);
 }
